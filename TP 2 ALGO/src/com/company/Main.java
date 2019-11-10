866d6677d36bbe8908aa7731e46e781b94e3d371
valentinflegon <43956710+valentinflegon@users.noreply.github.com>
2019-11-10 15:55:06 +0100

    Update Main.java

--- TP 2 ALGO/src/com/company/Main.java
+++ TP 2 ALGO/src/com/company/Main.java
@@ -1,16 +1,18 @@
 package com.company;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-       Automaton automate = new Automaton();
-      /*  automate.addWord("teamjul");
+    public static void main(String[] args) throws IOException {
+      Automaton automate = new Automaton();
+      System.out.println(automate.readText());
+     /*   automate.addWord("teamjul");
         automate.addWord("karris");
         automate.addWord("teamjoel");
         automate.display();
-        */
+*/
         String value = "0",texte;
         Scanner sc;
         System.out.println("Pour sortir rentrer * ");
@@ -21,12 +23,12 @@ public class Main {
             if (!value.equals("*"))
                 automate.addWord(value);
         } while (!value.equals("*"));
-        automate.display();
-        System.out.print("Rentrer un texte : ");
-        sc = new Scanner(System.in);
-        automate.text = sc.nextLine();
-        //automate.readText(automate.text);
-        automate.recherche(automate.text);
+       // automate.display();
+      //  System.out.print("Rentrer un texte : ");
+       // sc = new Scanner(System.in);
+      //  automate.text = sc.nextLine();
+        //automate.recherche(automate.text);
+        automate.recherche(automate.readText());
         // retourne les mots keyword present dans le texte
     }
 }
